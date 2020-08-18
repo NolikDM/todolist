b87eb10aa0bb5bb4418140fb667f88c304d9bef6
NolikDM <56555674+NolikDM@users.noreply.github.com>
2020-08-19 02:58:46 +0700

    EventBus

--- src/main.js
+++ src/main.js
@@ -1,32 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
 
-import { firebaseConfig } from "./firebase_config.js";
-import * as firebase from "firebase/app";
+window.eventBus = new Vue()
 
-// Initialize Firebase
-//firebase.config.initializeApp(firebaseConfig);
+Vue.config.productionTip = false
 
-Vue.config.productionTip = false;
-
-//let app;
-
-// firebase.auth().onAuthStateChanged(() => {
-//   if (!app) {
-//     app = new Vue({
-//       router,
-//       store,
-//       render: (h) => h(App),
-//     }).$mount("#app");
-//   }
-// });
-
+/* eslint-disable no-new */
 new Vue({
-      router,
-      store,
-      render: (h) => h(App),
-    }).$mount("#app");
-  )};
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
